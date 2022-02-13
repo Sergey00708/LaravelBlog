03f995749d99ae857104deca3dead088184cd810
Fenix00708 <Fenix00708@gmail.com>
2022-02-13 14:08:25 +0200

    lesson 3

--- database/migrations/2022_02_13_111644_create_posts_table.php
+++ database/migrations/2022_02_13_111644_create_posts_table.php
@@ -15,7 +15,13 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->unsignedInteger('category_id')->nullable();
             $table->timestamps();
+
+            $table->index('category_id', 'post_category_idx');
+            $table->foreign('category_id', 'post_category_fk')->on('categories')->references('id');
         });
     }
 
